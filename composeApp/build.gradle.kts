fb638aa35f5206e2527533c07d92641e84258de2
Jo Eungyeong <joeungyeong23@gmail.com>
2026-03-04 13:23:53 +0900

    feat: 컴포즈 기초 파일 분리

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -34,6 +34,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.material.icons.extended)
+            implementation(libs.compose.components.resources)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
