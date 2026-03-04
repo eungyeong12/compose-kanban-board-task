6e6509f05d30f78143468d816f0a243a41697b95
Jo Eungyeong <joeungyeong23@gmail.com>
2026-03-04 14:43:31 +0900

    feat: 태스크 추가 버튼 누르면 태스크 입력창 생성 구현

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,10 +1,61 @@
 package woowacourse.kanban.board
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kanbanboard.composeapp.generated.resources.Res
+import kanbanboard.composeapp.generated.resources.task_planet
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 @Preview(showBackground = true)
 fun App() {
+    val inputWindow = InputWindow()
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var showInputWindow = AddButton(modifier = Modifier.align(Alignment.Center))
+        if(showInputWindow) inputWindow.OpenInputWindow()
+    }
 
+}
+
+@Composable
+fun AddButton(modifier: Modifier): Boolean {
+    var showInputWindow by remember { mutableStateOf(false) }
+    Button(
+        onClick = { showInputWindow = !showInputWindow },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF5CFFD1),
+            contentColor = Color.Black
+        ),
+        modifier = modifier
+    ) {
+        Row {
+            Image(
+                painter = painterResource(Res.drawable.task_planet),
+                contentDescription = "",
+                modifier = Modifier.size(20.dp)
+            )
+            Text("추가")
+        }
+    }
+    return showInputWindow
 }
