f9cd5677ca59110db79b3202a6f2842c727fa147
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-03-07 02:41:51 +0900

    fix: CheckerView 임포트

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/CheckerTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/CheckerTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.runComposeUiTest
 import org.junit.Test
+import woowacourse.kanban.board.study.CheckerView
 
 @OptIn(ExperimentalTestApi::class)
 class CheckerTest {
