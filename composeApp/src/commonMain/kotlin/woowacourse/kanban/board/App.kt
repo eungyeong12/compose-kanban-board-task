239d55fd586e6ca3690c209378d533d28d27baea
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-03-07 21:12:42 +0900

    [1단계 - 칸반 보드 태스크(카드)] 엘리 미션 제출합니다. (#8)
    * feat: study 테스트 코드 추가
    
    * docs: 구현할 기능 작성
    
    * feat: 컴포즈 기초 구현
    
    * docs: 기능 목록 수정 및 예외 사항 추가
    
    * feat: 컴포즈 기초 파일 분리
    
    * feat: 태스크 추가 버튼 누르면 태스크 입력창 생성 구현
    
    * feat: 태스크 입력창 구현
    
    * feat: 태스크 카드 구현
    
    * feat: 태스크 카드 클래스 추가
    
    * fix: 태스크 카드 오류 수정
    
    * feat: 태그 개수 및 글자수 제한, 제목, 내용, 작성자 라인 수 제한
    
    * feat: 태스크 리스트 정렬 구현
    
    * fix: 태스크 목록과 입력 창 상태 관리 로직 수정
    
    * feat: Compose 실습
    
    * fix: CreateTaskCard 파일 분리 및 함수 분리, CreateTaskCard와 TaskCard를 taskcard 패키지로 이동
    
    * fix: App.kt에서 AddButton, GroupTheTaskCard 분리
    
    * refactor: 배경 색상 변경
    
    * refactor: 태그 ui 수정
    
    * refactor: import 정리
    
    * fix: showOpenWindow 상태 관리 로직 수정
    
    * refactor: 버튼 ui 수정
    
    * fix: taskCardGroup, showOpenWindow 상태 관리 로직 수정
    
    * refactor: Basics.kt 패키지를 study 안으로 이동
    
    * fix: 입력값 상태 관리 로직 수정 및 함수 분리
    
    * fix: CheckerView 임포트
    
    * fix: 함수명, 파일명을 명사형으로 변경
    
    * feat: Preview 함수 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,44 +1,43 @@
 package woowacourse.kanban.board
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kanbanboard.composeapp.generated.resources.Res
-import kanbanboard.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.component.AddButton
+import woowacourse.kanban.board.taskcard.TaskCardGroup
+import woowacourse.kanban.board.taskcard.TaskCardDto
+import woowacourse.kanban.board.taskcardcreation.InputWindow
 
+@Preview(showBackground = true)
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
-            }
-        }
+    var taskCardGroup by rememberSaveable { mutableStateOf(listOf<TaskCardDto>()) }
+    var showOpenWindow by rememberSaveable { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        TaskCardGroup(taskCardGroup)
+        AddButton(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            { showOpenWindow = true }
+        )
+        InputWindow(
+            modifier = Modifier.align(Alignment.Center),
+            { taskCardGroup = taskCardGroup + it },
+            showOpenWindow,
+            { showOpenWindow = false }
+        )
     }
 }
---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/AddButton.kt
@@ -0,0 +1,50 @@
+package woowacourse.kanban.board.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kanbanboard.composeapp.generated.resources.Res
+import kanbanboard.composeapp.generated.resources.task_planet
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun AddButton(
+    modifier: Modifier,
+    onValueChange: () -> Unit,
+) {
+    Button(
+        onClick = { onValueChange() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF5CFFD1),
+            contentColor = Color.Black,
+        ),
+        modifier = modifier
+    ) {
+        Row {
+            Image(
+                painter = painterResource(Res.drawable.task_planet),
+                contentDescription = "행성이 이미지",
+                modifier = Modifier.size(50.dp),
+            )
+            Text(
+                text = "추가",
+                fontSize = 50.sp,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AddButtonPreview() {
+    AddButton(modifier = Modifier, onValueChange = {})
+}
